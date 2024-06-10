566b28387f46ec979f66e8d2f4fbd198babadada
serbart20 <serbart.dev@gmail.com>
2024-06-10 13:01:09 +0200

    added test

--- pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.*;
 class OwnerSDJpaServiceTest {
 
     public static final String LAST_NAME = "Smith";
+    
     @Mock
     OwnerRepository ownerRepository;
 
--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfgpetclinic;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
